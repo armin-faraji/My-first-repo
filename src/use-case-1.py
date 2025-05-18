5c66de30d9280c17cac423401fb0eaa27fdfca27
armin-faraji <arminfaraji01@gmail.com>
2025-05-18 12:43:39 +0330

    added comment to use-case-1.py and added use-case-2.py

--- src/use-case-1.py
+++ src/use-case-1.py
@@ -1,3 +1,5 @@
+# We implement our array with Numpy array
+
 import numpy as np
 
 x = np.array([1, 2, 3])
---
+++ src/use-case-2.py
@@ -0,0 +1,6 @@
+# We implement our array with builtin Python array
+
+import array
+
+x = array.array('b', [1, 2, 3])
+print(x)
